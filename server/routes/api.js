d6e07bce79b2f9015516a82a3f69923b3b7a94e9
Samuel Windham <sam.windham@arrow.com>
2017-08-09 10:11:03 -0600

    added mongoDB availability. API catches errors correctly

--- server/routes/api.js
+++ server/routes/api.js
@@ -6,21 +6,29 @@ const router = express.Router();
 
 // declare axios for making http requests
 const axios = require('axios');
-const API = 'https://newsapi.org/v1/';
+const API = 'https://newsapi.org/v1';
 const apiKey = '6c6bcb21de2f4a5aaecedf0f98ee20ca';
 
 const source = 'the-new-york-times';
 
-
 // GET api listening
-router.get('/posts', (req, res) => {
-    axios.get(`${API}/articles?source=${source}&sortBy=latest&apiKey=${apiKey}`)
-        .then(response => {
-            res.status(200).json(response.data);
-        })
-        .catch(error => {
-            res.status(500).send(error);
-        });
-});
+router.get('/articles', (req, res) => {
+    console.log('Requesting articles from newsapi.org ...');
+    const url = `${API}/articles?source=${source}&sortBy=Popular&apiKey=${apiKey}`;
+
+    axios.get(url)
+    .then(response => {
+        console.log('Request successful.');
+        res.status(200).json(response.data.articles);
+    })
+    .catch(err => {
+        if(err instanceof axios.HttpError) {
+            res.status(500).send(err);
+        } else if(err instanceof Error) {
+            console.error("From api.js: " + err.message);
+        }
+    });
+
+})
 
 module.exports = router;
